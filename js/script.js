6f2adc8505f154d8280100290908324a510b48d0
Заместитель отдела : Захаров Р.Е <143989034+GermanTvardovski@users.noreply.github.com>
2024-04-07 22:33:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+jQuery(($) => {
+    $('.select').on('click', '.select__head', function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().fadeOut();
+        } else {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+            $(this).addClass('open');
+            $(this).next().fadeIn();
+        }
+    });
+
+    $('.select').on('click', '.select__item', function () {
+        $('.select__head').removeClass('open');
+        $(this).parent().fadeOut();
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.select').length) {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+        }
+    });
+
+    $.fn.setCursorPosition = function(pos) {
+        if ($(this).get(0).setSelectionRange) {
+          $(this).get(0).setSelectionRange(pos, pos);
+        } else if ($(this).get(0).createTextRange) {
+          var range = $(this).get(0).createTextRange();
+          range.collapse(true);
+          range.moveEnd('character', pos);
+          range.moveStart('character', pos);
+          range.select();
+        }
+    };
+
+    $("#phone").click(function(){
+        $(this).setCursorPosition(3);
+      }).mask("+7(999) 999-9999");
+
+    $('.panel-heading').click(function () {
+        $(this).toggleClass('in').next().slideToggle();
+        $('.panel-heading').not(this).removeClass('in').next().slideUp();
+    });
+});
